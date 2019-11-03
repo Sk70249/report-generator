8d99f2c054247a487d091fc639c695fca00f2870
pratik agrawal <46810588+Pkpratik@users.noreply.github.com>
2019-11-03 23:02:42 +0530

    Update pdf.py
    changes made accordingly

--- pdf.py
+++ pdf.py
@@ -1,5 +1,5 @@
-def genpdf(x):
-    from reportlab.pdfgen import canvas
+from reportlab.pdfgen import canvas
+def generate_pdf(text):
     c=canvas.Canvas("report.pdf")
-    c.drawString(0,830,x)
+    c.drawString(0,830,text)
     c.save()
